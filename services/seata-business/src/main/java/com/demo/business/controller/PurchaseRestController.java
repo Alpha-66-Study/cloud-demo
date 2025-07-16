929b264e9e75c124e9a695e40794207efed0a1b9
honghui <wanghonghui_work@163.com>
2025-07-16 09:29:20 +0800

    fix

--- services/seata-business/src/main/java/com/demo/business/controller/PurchaseRestController.java
+++ services/seata-business/src/main/java/com/demo/business/controller/PurchaseRestController.java
@@ -2,7 +2,6 @@ package com.demo.business.controller;
 
 import com.demo.business.service.BusinessService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- services/seata-storage/src/main/java/com/demo/storage/service/impl/StorageServiceImpl.java
+++ services/seata-storage/src/main/java/com/demo/storage/service/impl/StorageServiceImpl.java
@@ -3,7 +3,6 @@ package com.demo.storage.service.impl;
 import com.demo.storage.mapper.StorageTblMapper;
 import com.demo.storage.service.StorageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
